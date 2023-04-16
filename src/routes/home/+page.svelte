d0c1c1bbe3e4f86adaac9ceec70bdab6f2b0edd1
mp281x <paludgnachmatteo.dev@gmail.com>
2023-04-16 16:17:09 +0200

    test fix mobile 3

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,15 +1,6 @@
 <script>
-	import { onMount } from 'svelte';
 	import Hero from './hero.svelte';
 	import Tecnology from './tecnology.svelte';
-
-	onMount(() => {
-		window.onload = function () {
-			setTimeout(function () {
-				window.scrollTo(0, 1);
-			}, 0);
-		};
-	});
 </script>
 
 <div id="scrollSnap">
@@ -21,7 +12,7 @@
 <style lang="postcss">
 	#scrollSnap {
 		/* add the scroll snap functionality */
-		height: 100vh;
+		height: 100dvh;
 		scroll-snap-type: y proximity;
 		overflow-x: scroll;
 
