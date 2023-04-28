224e9c3812d645b1f6e02ac8cba86b48b9e1e37a
mp281x <paludgnachmatteo.dev@gmail.com>
2023-04-28 10:35:45 +0200

    fix scroll animation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,21 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import { afterNavigate, beforeNavigate } from '$app/navigation';
-
-	// import the scc with the tailwind config and the custom class
 	import '../app.css';
 
-	onMount(() => {
+	// disable smooth scroll and reset animation on page change
+	beforeNavigate(() => (document.getElementsByTagName('html')[0].style.scrollBehavior = 'auto'));
+	afterNavigate(() => {
+		document.getElementsByTagName('html')[0].style.scrollBehavior = 'smooth';
+
 		/* when a element with the data attribute "visible" is visible
 		on the screen change the attribute to run the css animation */
-		const observer = new IntersectionObserver((entries) => {
-			entries.forEach((entry) => {
-				if (entry.isIntersecting) entry.target.setAttribute('data-visible', 'true');
-			});
-		});
-
-		document.querySelectorAll('[data-visible="false"]').forEach((el) => observer.observe(el));
-
-		// disable smooth scroll and reset animation on page change
-		beforeNavigate(() => {
-			document.getElementsByTagName('html')[0].style.scrollBehavior = 'auto';
-			document.querySelectorAll('[data-visible="false"]').forEach((e) => e.setAttribute('data-visible', 'false'));
-		});
-		afterNavigate(() => (document.getElementsByTagName('html')[0].style.scrollBehavior = 'smooth'));
+		document.querySelectorAll('[data-visible="false"]').forEach((el) =>
+			new IntersectionObserver((entries) => {
+				entries.forEach((entry) => {
+					if (entry.isIntersecting) entry.target.setAttribute('data-visible', 'true');
+				});
+			}).observe(el)
+		);
 	});
 </script>
 
