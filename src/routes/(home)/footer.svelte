3f5b29e31e5390149e4a88a98671eaa5d5044634
= <=>
2023-05-31 17:14:25 +0000

    added new migration script (to fix)

--- src/routes/(home)/footer.svelte
+++ src/routes/(home)/footer.svelte
@@ -1,1 +1,5 @@
-<footer class="flex h-20 w-full items-center bg-accent p-5 text-xl font-bold text-white">Paludgnach Matteo</footer>
+<footer class="flex h-20 w-full items-center justify-between bg-accent p-5 text-xl font-bold text-white">
+	<div>Paludgnach Matteo</div>
+	<a href="https://github.com/MP281X" target="_blank"><img src="/img/devops/github.svg" alt="github" class="h-10" /></a>
+	<!-- <div class="flex gap-5 bg-orange-500">a</div> -->
+</footer>
