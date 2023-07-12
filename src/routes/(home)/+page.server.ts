d283098ff82be86276f106862949ef21b38f384e
= <=>
2023-07-12 21:14:15 +0200

    fix skill order bug

--- src/routes/(home)/+page.server.ts
+++ src/routes/(home)/+page.server.ts
@@ -3,11 +3,11 @@ import { db } from '$lib/db';
 export const load = async () => {
 	const skill = await db.selectFrom('skill').selectAll().execute();
 
-	const backend = skill.filter((x) => x.category === 'backend');
 	const frontend = skill.filter((x) => x.category === 'frontend');
+	const backend = skill.filter((x) => x.category === 'backend');
 	const devops = skill.filter((x) => x.category === 'devops');
 
 	const project = await db.selectFrom('project').select(['project', 'decription', 'url', 'stack']).execute();
 
-	return { skills: { backend, frontend, devops }, project };
+	return { skills: { frontend, backend, devops }, project };
 };
