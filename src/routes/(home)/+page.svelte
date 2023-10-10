684be3729ff69381026d94acddf624754ac7473a
mp281x <paldugnachmatteo.dev@gmail.com>
2023-10-10 13:30:46 +0200

    removed redis

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -1,26 +1,9 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import type { sseData } from '$lib/sseHandler.server';
-
 	import Navbar from '$lib/navbar.svelte';
 	import Hero from './hero.svelte';
 	import Skills from './skills.svelte';
 	import Project from './project.svelte';
 	import Footer from './footer.svelte';
-
-	onMount(() => {
-		// connect to the sse server
-		const eventSource = new EventSource('/sse');
-
-		// listen for new message
-		eventSource.addEventListener('message', (event) => {
-			// return the message
-			try {
-				const data = JSON.parse(event.data) as sseData;
-				console.log(data.message);
-			} catch {} // eslint-disable-line no-empty
-		});
-	});
 
 	export let data;
 </script>
--- src/routes/sse/+server.ts
+++
@@ -1,6 +0,0 @@
-import { sseServer } from '$lib/sseHandler.server';
-import type { RequestHandler } from '@sveltejs/kit';
-
-export const GET: RequestHandler = async () => {
-	return sseServer('sse');
-};
