35c8174bd6c766b004827660f9a88ca885987185
mp281x <paldugnachmatteo.dev@gmail.com>
2023-10-10 17:45:50 +0200

    added the button to download the cv in the footer

--- src/routes/(home)/footer.svelte
+++ src/routes/(home)/footer.svelte
@@ -1,5 +1,7 @@
 <footer class="flex h-20 w-full items-center justify-between bg-accent p-5 text-xl font-bold text-white">
 	<div>Paludgnach Matteo</div>
-	<a href="https://github.com/MP281X" target="_blank"><img src="/img/devops/github.svg" alt="github" class="h-10" /></a>
-	<!-- <div class="flex gap-5 bg-orange-500">a</div> -->
+	<div class="flex flex-row gap-5">
+		<a href="/Paludgnach_Matteo_CV.pdf" target="_blank"><img src="/img/cv.svg" alt="cv" class="h-10" /></a>
+		<a href="https://github.com/MP281X" target="_blank"><img src="/img/devops/github.svg" alt="github" class="h-10" /></a>
+	</div>
 </footer>
