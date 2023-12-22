79fd89fe6962bc52032ffd13b5b9a047b051a0c5
= <=>
2023-12-22 15:15:33 +0100

    added the docker-compose

--- script/migrate.sh
+++ script/migrate.sh
@@ -9,7 +9,7 @@ atlas schema clean --auto-approve -u $POSTGRES_URL;
 atlas schema apply --auto-approve -u $POSTGRES_URL --to file://db/schema.hcl;
 
 # generate the db client
-npx kysely-codegen --dialect postgres --out-file ./src/lib/db.d.ts --url $POSTGRES_URL;
+bun x kysely-codegen --dialect postgres --out-file ./src/lib/db.d.ts --url $POSTGRES_URL;
 
 # run the seed script
-export POSTGRES_URL=$POSTGRES_URL && npx ts-node-esm db/seed.ts;
+export POSTGRES_URL=$POSTGRES_URL && bun db/seed.ts;
